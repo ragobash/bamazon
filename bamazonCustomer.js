0809f7c187bc1a0d510efa1c5cbee07e242b4fb5
Ryan Harris <ragobash@gmail.com>
2019-09-08 14:47:59 -0500

    The first (minimum requirement) is working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,52 +1,96 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+var mysql = require('mysql');
+var inquirer = require('inquirer');
 
-// create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-
-    // Your port; if not 3306
+    host: 'localhost',
     port: 3306,
 
-    // Your username
-    user: "root",
-
-    // Your password
-    password: "R1y9A9n0",
-    database: "bamazon"
+    user: 'root',
+    password: 'R1y9A9n0',
+    database: 'bamazon'
 });
 
-// connect to the mysql server and sql database
-connection.connect(function (err) {
+connection.connect(err => {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    start();
-});
+    console.log("connected")
+    start ();
+})
 
-// function which prompts the user for what action they should take
 function start() {
-    // query the database for all items for sale
-    connection.query("SELECT * FROM products", function (err, results) {
-        if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-            .prompt([
-                   {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].product_name);
+    connection.query(
+        "SELECT * FROM products", (err, results) => {
+            if (err) throw err;
+            inquirer
+                .prompt([{
+                        name: 'choices',
+                        type: 'list',
+                        choices: () => {
+                            var choiceArr = [];
+                            for (var i = 0; i < results.length; i++) {
+                                choiceArr.push(results[i].product_name)
+                            }
+                            return choiceArr
+                        },
+                        message: "Which item would you like to buy?"
+                    },
+                    {
+                        name: 'amount',
+                        type: 'input',
+                        message: 'How many would you like to buy?'
+                    }
+                ])
+                .then((answer) => {
+                    var chosenProduct;
+                    for (var i = 0; i < results.length; i++) {
+                        if (results[i].product_name === answer.choices) {
+                            chosenProduct = results[i];
                         }
-                        return choiceArray;
-                        }
-                   },
-                   {
-                    name: "item",
-                    type: "input",
-                    message: "Please insert the item id for the item you would like to buy."
-                   },
-                   ])
-                })
-            };
+                    }
+                    if (chosenProduct.stock_quantity > parseInt(answer.amount)) {
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?", [{
+                                    stock_quantity: chosenProduct.stock_quantity - answer.amount
+                                },
+                                {
+                                    item_id: chosenProduct.item_id
+                                }
+                            ],
+                            (err) => {
+                                if (err) throw err;
+                                console.log('Your order has been placed!')
+                                shopAgain();
+                            }
+                        );
+                    }
+                });
+        });
+}
+
+function startShopping() {
+    console.log('Here are the current items available:')
+    query = connection.query(
+        "SELECT * FROM products",
+        (err, res) => {
+            if (err) throw err;
+            for (var i = 0; i < res.length; i++){
+                console.log(`Id: ${res[i].item_id}: Name: ${res[i].product_name}: Dep: ${res[i].department_name}: Price: ${res[i].price}: In Stock: ${res[i].stock_quantity}:`)
+            }
+            start();
+        }
+    )
+};
+var shopAgain = () => {
+    inquirer
+        .prompt([{
+            type: 'confirm',
+            name: 'shop',
+            message: "Would you like to start shopping?"
+        }]).then((answer) => {
+            if (answer.shop === true) {
+                startShopping();
+            } else {
+                console.log('Thank you! Have a good day!');
+                connection.end();
+            }
+        })
+}
